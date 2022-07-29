89389feda77c80649057eec188facfcfecab44a2
Tim Kersey <217+tkersey@users.noreply.github.com>
2022-07-29 21:51:37 +0000

    kotlin-debug-adapter: add 0.4.3 bottle.

--- Formula/kotlin-debug-adapter.rb
+++ Formula/kotlin-debug-adapter.rb
@@ -5,6 +5,12 @@ class KotlinDebugAdapter < Formula
   sha256 "9ad4db7e8877ed73d0b14c682f8bb8c4aed62918e3d936f0eb368052e2de2a8d"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/tkersey/homebrew-tap/releases/download/kotlin-debug-adapter-0.4.3"
+    sha256 cellar: :any_skip_relocation, big_sur:      "a6ff3a918dbcb0f128e0003063f3e1e9dccb5c26e965c563112921385308ea1a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "1bf9dfc572ce0eba73a76067d9fba26a7320242260cec797adca691464618e87"
+  end
+
   depends_on "gradle" => :build
   depends_on "openjdk"
 
