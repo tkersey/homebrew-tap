52e0bd91ea5cb019d9f6a8679d067796dd07e4ae
Tim Kersey <t@k-t.im>
2026-02-25 18:27:27 -0800

    cas 0.2.8

--- Formula/cas.rb
+++ Formula/cas.rb
@@ -1,21 +1,19 @@
 class Cas < Formula
   desc "Zig CLI helpers for Codex app-server orchestration"
   homepage "https://github.com/tkersey/skills-zig"
-  version "0.2.7"
+  version "0.2.8"
 
   on_macos do
     depends_on arch: :arm64
     url "https://github.com/tkersey/skills-zig/releases/download/cas-v#{version}/cas-v#{version}-darwin-arm64.tar.gz"
-    sha256 "2df3aef076b3f86b4d772f1e8bb08bf2bc9f465a8ef832e48d05351a9bc7617a"
+    sha256 "a2ee09ecfb42342dafcbd045c9cffbe54e86f990d8357eea13cf76eb550e4935"
   end
 
   on_linux do
     depends_on arch: :x86_64
     url "https://github.com/tkersey/skills-zig/releases/download/cas-v#{version}/cas-v#{version}-linux-x86_64.tar.gz"
-    sha256 "a129cd4615197f7b8941a644c1b40c4be85cd78159dd15505c9db2613ee5ff0f"
+    sha256 "16a3e460bcdccd447ea1dc425b48fc79d8fb48a5be4402336ab1e50fede9a8be"
   end
-
-  depends_on "node"
 
   def install
     bin.install "cas"
