11610602edfe6878230956ad03d09f6be1f95aed
Tim Kersey <t@k-t.im>
2026-02-27 12:23:13 -0800

    lift: fix strict audit formula layout

--- Formula/lift.rb
+++ Formula/lift.rb
@@ -3,13 +3,11 @@ class Lift < Formula
   homepage "https://github.com/tkersey/skills-zig"
   version "0.2.7"
 
-  on_macos do
+  if OS.mac?
     depends_on arch: :arm64
     url "https://github.com/tkersey/skills-zig/releases/download/lift-v#{version}/lift-v#{version}-darwin-arm64.tar.gz"
     sha256 "7d4212ab0ce328835395f1284b345816187fe81f50e5270831a60680b7694439"
-  end
-
-  on_linux do
+  else
     depends_on arch: :x86_64
     url "https://github.com/tkersey/skills-zig/releases/download/lift-v#{version}/lift-v#{version}-linux-x86_64.tar.gz"
     sha256 "e047581bbe3c872aa2cf77fd5b18a83232363cab9c89f65634ee7dcf2c06661c"
