ac14792ba35ada372cb9bb8bbd3e15e7c29921a2
Tim Kersey <t@k-t.im>
2026-02-26 19:32:28 -0800

    learnings: bump formula to 0.1.6

--- Formula/learnings.rb
+++ Formula/learnings.rb
@@ -1,19 +1,23 @@
 class Learnings < Formula
   desc "Zig CLIs for Codex learnings capture workflows"
   homepage "https://github.com/tkersey/skills-zig"
-  version "0.1.5"
+  version "0.1.6"
   license "MIT"
+
+  if OS.mac?
+    url "https://github.com/tkersey/skills-zig/releases/download/learnings-v#{version}/learnings-v#{version}-darwin-arm64.tar.gz"
+    sha256 "e715005be3d72234fd6a5afa002098f1609a98b8c43aea7984a1038a7f072815"
+  else
+    url "https://github.com/tkersey/skills-zig/releases/download/learnings-v#{version}/learnings-v#{version}-linux-x86_64.tar.gz"
+    sha256 "1a2ed74b6939744a4ebdc864b227ab53feb5aaf391c24e8016e748826edfbff6"
+  end
 
   on_macos do
     depends_on arch: :arm64
-    url "https://github.com/tkersey/skills-zig/releases/download/learnings-v#{version}/learnings-v#{version}-darwin-arm64.tar.gz"
-    sha256 "09a442ab4f7ef33ade4445ac6bb878cee9e0e7f6f82faab12132679482d0371b"
   end
 
   on_linux do
     depends_on arch: :x86_64
-    url "https://github.com/tkersey/skills-zig/releases/download/learnings-v#{version}/learnings-v#{version}-linux-x86_64.tar.gz"
-    sha256 "7f9ed0151859c8c304fc0f72d5175de046dedfc007541d6a0796775e520f1ee5"
   end
 
   def install
