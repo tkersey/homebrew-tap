dc49567376a5a534b31eb58455e9a942c1e3434e
Tim Kersey <t@k-t.im>
2026-02-28 10:09:56 -0800

    mesh: bump to 0.1.1

--- Formula/mesh.rb
+++ Formula/mesh.rb
@@ -1,15 +1,15 @@
 class Mesh < Formula
   desc "Zig CLI for plan-driven orchestration helpers"
   homepage "https://github.com/tkersey/skills-zig"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   if OS.mac?
     url "https://github.com/tkersey/skills-zig/releases/download/mesh-v#{version}/mesh-v#{version}-darwin-arm64.tar.gz"
-    sha256 "31968f43d8f641dd2717fd84401cd68cbc70f4493d14e84c24a03a303c1f0390"
+    sha256 "1603b7b692222d5942f8151883e66b85426feed8eeebaa39e85daf20d801cbad"
   else
     url "https://github.com/tkersey/skills-zig/releases/download/mesh-v#{version}/mesh-v#{version}-linux-x86_64.tar.gz"
-    sha256 "606fc5935a82da4ed06efd13a5d1a5399c0ea8366cd5ac1be80ffd908b57064d"
+    sha256 "303891a13d71f43afcf7b53a3647eb32bdc2ae1867caf44318f142de70028693"
   end
 
   on_macos do
