e8f33fc3b44aca60229e53ebc5fcd510742f6fcd
Tim Kersey <t@k-t.im>
2026-03-04 12:38:09 -0800

    mesh 0.1.4

--- Formula/mesh.rb
+++ Formula/mesh.rb
@@ -1,15 +1,15 @@
 class Mesh < Formula
   desc "Zig CLI for plan-driven orchestration helpers"
   homepage "https://github.com/tkersey/skills-zig"
-  version "0.1.2"
+  version "0.1.4"
   license "MIT"
 
   if OS.mac?
     url "https://github.com/tkersey/skills-zig/releases/download/mesh-v#{version}/mesh-v#{version}-darwin-arm64.tar.gz"
-    sha256 "d17ec4a8bf9eaf648d8a381d6c9ddbe664b779c5b5502209ed06d74accb8e7b5"
+    sha256 "02aeabea03cb4bda7793b0a8f95a322541a1837dd5172a8a40bf4d85605c21c8"
   else
     url "https://github.com/tkersey/skills-zig/releases/download/mesh-v#{version}/mesh-v#{version}-linux-x86_64.tar.gz"
-    sha256 "efa5faa9206950f3eb9fb88730468f58562dd891e6c7e2f9624276c36aac3163"
+    sha256 "c862858c131ceed766b6db56f305bc8f5bb97bf246f199be13c84fe599e7cf91"
   end
 
   on_macos do
