50af716fc159013bb77a917035ff859a02632f2c
Tim Kersey <t@k-t.im>
2026-03-08 19:31:45 -0700

    st: bump formula to 0.1.3

--- Formula/st.rb
+++ Formula/st.rb
@@ -1,15 +1,15 @@
 class St < Formula
   desc "Zig CLI for dependency-aware durable task plans"
   homepage "https://github.com/tkersey/skills-zig"
-  version "0.1.2"
+  version "0.1.3"
   license "MIT"
 
   if OS.mac?
     url "https://github.com/tkersey/skills-zig/releases/download/st-v#{version}/st-v#{version}-darwin-arm64.tar.gz"
-    sha256 "9d685f783d8ba3c3ae30a005d21267b2369aac0cba31716572fe1f7e5ff0ff4e"
+    sha256 "118b707243ecc34ecd04adc09599b33e66a51146ab9815118a92a8426c4f67f7"
   else
     url "https://github.com/tkersey/skills-zig/releases/download/st-v#{version}/st-v#{version}-linux-x86_64.tar.gz"
-    sha256 "ced27fe38dc9f95d5ff768570e4e24434a90aa7837a2fc6398bce762cf0c73f4"
+    sha256 "55e9ba2e4269ec9ff051a4745f577e157f3957aa4c1e6db2fb8a37fc961f79b2"
   end
 
   on_macos do
