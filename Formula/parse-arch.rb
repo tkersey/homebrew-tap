7b8f832ef3f0c1efb196c024c32116cddd10a822
Tim Kersey <t@k-t.im>
2026-03-13 07:50:24 -0700

    Bump parse-arch to 0.1.2

--- Formula/parse-arch.rb
+++ Formula/parse-arch.rb
@@ -1,15 +1,15 @@
 class ParseArch < Formula
   desc "Zig CLI for repository architecture signal collection"
   homepage "https://github.com/tkersey/skills-zig"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
 
   if OS.mac?
     url "https://github.com/tkersey/skills-zig/releases/download/parse-arch-v#{version}/parse-arch-v#{version}-darwin-arm64.tar.gz"
-    sha256 "bffce652cec16c8eeaec6b3217dcaba5597d41b0db90ed4044065f76176488e4"
+    sha256 "f7b2694cad2c8749e78d0ae264df6d7d7eb7a9df899b5ac49474e62aa0c400ec"
   else
     url "https://github.com/tkersey/skills-zig/releases/download/parse-arch-v#{version}/parse-arch-v#{version}-linux-x86_64.tar.gz"
-    sha256 "14e9515ef12c05f780cb50aa13c606e05ef415a4076fb06f5ea1b81e0a7e8210"
+    sha256 "dc9b1ffd914df400901f4c66102618b82843f0323d92c28dad152c01be54269b"
   end
 
   on_macos do
