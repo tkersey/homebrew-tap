983d14d9b369e602cdce259b9d694fa7d032c04a
Tim Kersey <t@k-t.im>
2026-04-08 21:16:49 -0700

    seq: bump to 0.2.24

--- Formula/seq.rb
+++ Formula/seq.rb
@@ -1,17 +1,17 @@
 class Seq < Formula
   desc "Zig CLI for mining Codex session and memory artifacts"
   homepage "https://github.com/tkersey/skills-zig"
-  version "0.2.23"
+  version "0.2.24"
   license "MIT"
 
   if OS.mac?
     depends_on arch: :arm64
     url "https://github.com/tkersey/skills-zig/releases/download/seq-v#{version}/seq-v#{version}-darwin-arm64.tar.gz"
-    sha256 "fd2e90395290385e5ce4df3d23a0ab47dd282575c9ab35c4b443a0cab7fd344a"
+    sha256 "ab45b6b8c059f2918a166c91d9da1bac6e88783fa6f081fe81c652b6cf1b5811"
   else
     depends_on arch: :x86_64
     url "https://github.com/tkersey/skills-zig/releases/download/seq-v#{version}/seq-v#{version}-linux-x86_64.tar.gz"
-    sha256 "46b4e65378c5f1e16c4cbc58a32b8d0b16788694ef54c1b6715536cf17fee2f2"
+    sha256 "261c3a95bced4621e16f8f73dd73959faaea072b0d0aa22f84a2d2fd9c9518f5"
   end
 
   def install
