2caf1ce320363f456943a072a979b749a0477a00
Tim Kersey <t@k-t.im>
2026-04-13 06:16:02 -0700

    release: retarget lift formula to 0.2.10

--- Formula/lift.rb
+++ Formula/lift.rb
@@ -1,16 +1,16 @@
 class Lift < Formula
   desc "Zig CLI helpers for performance measurement workflows"
   homepage "https://github.com/tkersey/skills-zig"
-  version "0.2.9"
+  version "0.2.10"
 
   if OS.mac?
     depends_on arch: :arm64
     url "https://github.com/tkersey/skills-zig/releases/download/lift-v#{version}/lift-v#{version}-darwin-arm64.tar.gz"
-    sha256 "fb10fd1304afdf320252e26361bdcc1caaf89e1e64cc9e1548936700ff632499"
+    sha256 "2f2f83acb4eef2918de655427a590be77c8525830b7bc284be41af411269ba72"
   else
     depends_on arch: :x86_64
     url "https://github.com/tkersey/skills-zig/releases/download/lift-v#{version}/lift-v#{version}-linux-x86_64.tar.gz"
-    sha256 "12eba003450da1dc65334ebd992b3e51357262b00b926080c7351c666e0d6a99"
+    sha256 "2d1b304b89bf95ecd4805847028b9c2f19e58b895dcac4efbc379eea76fb195a"
   end
 
   def install
