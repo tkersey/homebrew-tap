c95dde0edee02c26c3f472b6f23cec93b2dd6cdc
Tim Kersey <t@k-t.im>
2026-04-14 14:58:18 -0700

    st: release 0.1.8

--- Formula/st.rb
+++ Formula/st.rb
@@ -1,15 +1,15 @@
 class St < Formula
   desc "Zig CLI for dependency-aware durable task plans"
   homepage "https://github.com/tkersey/skills-zig"
-  version "0.1.7"
+  version "0.1.8"
   license "MIT"
 
   if OS.mac?
     url "https://github.com/tkersey/skills-zig/releases/download/st-v#{version}/st-v#{version}-darwin-arm64.tar.gz"
-    sha256 "811c56b7a7b22eea6997d67640ea7b12ee5407746bfc05dc2c1e63d34979e7f9"
+    sha256 "94d489ae3dc485752301d686386f9bfe92c34a5dcf7657cc5475655e287e396d"
   else
     url "https://github.com/tkersey/skills-zig/releases/download/st-v#{version}/st-v#{version}-linux-x86_64.tar.gz"
-    sha256 "013f457782a05a9db61eecd8aacd0b6bce47391f4d9bd1d695f575f6f2f82805"
+    sha256 "249dd1ed32f48775c69b5ee79e30e107c3eaa4b3efe5231be4d738a5e4cee407"
   end
 
   on_macos do
@@ -28,6 +28,7 @@ class St < Formula
     help = shell_output("#{bin}/st --help")
     assert_match "Manage dependency-aware JSONL v3 plan state.", help
     assert_match "import-orchplan", help
+    assert_match "import-update-plan", help
     assert_match "import-mesh-results", help
   end
 end
