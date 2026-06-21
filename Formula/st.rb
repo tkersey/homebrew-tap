96296bdfb578ff5bdb2b2756c19024fa3e8a57a5
Tim Kersey <t@k-t.im>
2026-06-20 17:25:00 -0700

    Update st to 0.4.5

--- Formula/st.rb
+++ Formula/st.rb
@@ -1,15 +1,15 @@
 class St < Formula
   desc "Zig CLI for dependency-aware durable task plans"
   homepage "https://github.com/tkersey/skills-zig"
-  version "0.4.3"
+  version "0.4.5"
   license "MIT"
 
   if OS.mac?
     url "https://github.com/tkersey/skills-zig/releases/download/st-v#{version}/st-v#{version}-darwin-arm64.tar.gz"
-    sha256 "4c57d7dccfe1c4660c26412395290b77642e305f32610a9d0f0cce8253999417"
+    sha256 "5dc6db62e5cdf9e0f9c12b58a38174bde1a9e88f7a2cd4e2e2be5854d232ce2a"
   else
     url "https://github.com/tkersey/skills-zig/releases/download/st-v#{version}/st-v#{version}-linux-x86_64.tar.gz"
-    sha256 "624624009cd8ef11e401df54c00afee7c3e10ab06e7f37063a8a5ce926b68b18"
+    sha256 "34d36494897bc2c9ee3fbeac7346d9a8df333d0575b3f42947a85b9c79071a63"
   end
 
   on_macos do
